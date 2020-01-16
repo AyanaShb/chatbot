3bca96655f550a3f04e14c2aa177103533d8938c
Ayana.shahab <37789259+AyanaShb@users.noreply.github.com>
2020-01-16 12:12:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def callback():
     return 'OK'
 @handler.add(JoinEvent)
 def jhjhjh(event):
-    print(event)
+    print("kontolllll")
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text.lower()
