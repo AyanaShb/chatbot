14d1676448d16f6f35eeb26f7055f35b3aa0c1e1
Ayana.shahab <37789259+AyanaShb@users.noreply.github.com>
2020-01-16 11:34:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,8 @@ def callback():
     try:handler.handle(body, signature)
     except InvalidSignatureError:abort(400)
     return 'OK'
+@handler.add(JoinEvent)
+print("hhh")
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text.lower()
